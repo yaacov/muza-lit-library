2a9d4237b51f30c60af5e6416bc3f4f620055724
Ido Goldblatt <ido.godlblat@gmail.com>
2025-03-03 18:10:27 +0200

    only song Line

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export { MusicSection } from "./MusicSection";
 export { MusicSidebar } from "./MusicSidebar";
 export { MusicTopbar } from "./MusicTopbar";
 export { VolumeControl } from "./VolumeControl";
-export { MusicPlayer } from "./MusicPlayer";
 export { SongLine } from "./SongLine"
